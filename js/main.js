92a1754dab5b4c7201a3c563b6adadff99269494
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-07-11 19:43:45 +0200

    add js and css bonus

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const showsList = document.querySelector ('.js-list-movies');
 const favoriteList = document.querySelector ('.list-fav-movie');
 const urlNullShowImage =
   'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const favTitle = document.querySelector ('.js-fav-tittle');
 
 function clickButton () {
   fetch (apiUrl + searchInput.value)
@@ -42,7 +43,7 @@ function printShows () {
       codeHTML += `<li class="js-show show favorite-color" id=${showInfo.show.id}>`;
     }
     codeHTML += `<img class="js-img-show img-show" src = ${getImage (showInfo)} alt='no image found'>`;
-    codeHTML += `<p class="js-img-name img-name">${showInfo.show.name}</p>`;
+    codeHTML += `<p class="js-show-name show-name">${showInfo.show.name}</p>`;
     codeHTML += `</li class="js-show show">`;
   }
   showsList.innerHTML = codeHTML;
@@ -84,12 +85,25 @@ function printShowsFavorites () {
 
   let codeFavoritesHTML = '';
   for (let favorite of favorites) {
-    codeFavoritesHTML += `<li class="js-show show" id=${favorite.show.id}>`;
-    codeFavoritesHTML += `<img class="js-img-show img-show" src = ${getImage (favorite)} alt='no image found'>`;
-    codeFavoritesHTML += `<p class="js-img-name img-name">${favorite.show.name}</p>`;
-    codeFavoritesHTML += `</li class="js-show show">`;
+    codeFavoritesHTML += `<li class="js-favorite-show favorite-show" id=${favorite.show.id}>`;
+    codeFavoritesHTML += `<img class="js-img-favorite-show img-favorite-show" src = ${getImage (favorite)} alt='no image found'> `;
+    codeFavoritesHTML += `<i class="far fa-times-circle times-circle" id=${favorite.show.id}></i>`;
+    codeFavoritesHTML += `<p class="js-show-name show-name">${favorite.show.name}</p>`;
+    codeFavoritesHTML += `</li class="js-favorite-show favorite-show">`;
+  }
+
+  if (favorites.length > 0) {
+    codeFavoritesHTML += `<button class="js-remove-all-btn remove-all-btn">Borrar todas</button>`;
   }
   favoriteList.innerHTML = codeFavoritesHTML;
+  if (favorites.length > 0) {
+    favTitle.classList.remove ('hidden');
+  } else {
+    favTitle.classList.add ('hidden');
+  }
+
+  listenButtonRemoveFavorite ();
+  listenRemoveAll ();
 }
 
 const updateLocalStorage = () => {
@@ -102,6 +116,39 @@ const getFromLocalStorage = () => {
     favorites = data;
   }
 };
+
+function listenButtonRemoveFavorite () {
+  const removeButtons = document.querySelectorAll ('.times-circle');
+  for (let removeButton of removeButtons) {
+    removeButton.addEventListener ('click', clickRemoveButton);
+  }
+}
+
+function clickRemoveButton () {
+  const clickedShowId = parseInt (event.currentTarget.id);
+  const favorite = favorites.find (
+    favoritesIDItem => favoritesIDItem.show.id === clickedShowId
+  );
+  favorites.splice (favorites.indexOf (favorite), 1);
+
+  updateLocalStorage ();
+  printShowsFavorites ();
+  printShows ();
+}
+
+function listenRemoveAll () {
+  if (favorites.length > 0) {
+    const removeAllButton = document.querySelector ('.js-remove-all-btn');
+    removeAllButton.addEventListener ('click', clickRemoveAll);
+  }
+}
+
+function clickRemoveAll () {
+  favorites = [];
+  updateLocalStorage ();
+  printShowsFavorites ();
+  printShows ();
+}
 
 printShowsFavorites ();
 searchButton.addEventListener ('click', clickButton);
